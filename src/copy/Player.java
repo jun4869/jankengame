d183c34ae13de7b467d3a569a7fba65bdba30f5a
jun4869 <jun2194k@gmail.com>
2021-04-07 16:01:41 +0900

    test

---
+++ src/copy/Player.java
@@ -0,0 +1,50 @@
+package copy;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class Player implements ActionListener{
+	public static void createButton(JPanel footerPanel) {
+		//ボタン表示
+		JButton btnGu = new JButton("グー");
+		btnGu = setButton(btnGu);
+		footerPanel.add(btnGu, BorderLayout.WEST);
+		
+		JButton btnChoki = new JButton("チョキ");
+		btnChoki = setButton(btnChoki);
+		footerPanel.add(btnChoki, BorderLayout.CENTER);
+		
+		JButton btnPa = new JButton("パー");
+		btnPa = setButton(btnPa);
+		footerPanel.add(btnPa, BorderLayout.EAST);
+	}
+	public static JButton setButton(JButton button) {
+		int buttonSizeX = (640 - 20)/3;
+		Dimension buttonDimension = new Dimension(buttonSizeX, 50);
+		button.setPreferredSize(buttonDimension);
+		Font buttonFont = new Font("HG行書体",Font.PLAIN,24);
+		button.setFont(buttonFont);
+		
+		Player player = new Player();
+		button.addActionListener(player);
+		
+		return(button);
+	}
+	public void actionPerformed(ActionEvent e) {
+		String command = e.getActionCommand();
+		int playerHand = 0;
+		if(command.equals("グー"))playerHand = 1;
+		else if(command.equals("チョキ"))playerHand = 2;
+		else if(command.equals("パー"))playerHand = 3;
+		
+		if(playerHand == 1)Panel.contentsLabel.setText("グー");
+		if(playerHand == 2)Panel.contentsLabel.setText("チョキ");
+		if(playerHand == 3)Panel.contentsLabel.setText("パー");
+	}
+}
